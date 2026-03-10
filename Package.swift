9b0a66a0a6bcc8ea1aa1d7d35282467000df6a52
nancyP <inancyp@outlook.com>
2026-03-09 23:10:47 -0400

    change in git link

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
 
         .binaryTarget(
             name: "AzureCommunicationCalling",
-            url: "https://github.com/Azure/Communication/releases/download/1.0.0/AzureCommunicationCalling-2.18.2-beta.2.zip",
+            url: "https://github.com/inancyGit/SPM-CallingTest/releases/download/1.0.0/AzureCommunicationCalling-2.18.2-beta.2.zip",
             checksum: "96b0faec320b9d6f3730826ad4d7de5933a275a2cbd27cfa27d6913003ea5c85"
         ),
         .binaryTarget(
